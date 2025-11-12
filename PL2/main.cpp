43791f18d5f702c73f21f9394606a5147df379d2
Fantom <emanuel.baciu@edu.uah.es>
2025-11-12 19:53:47 +0100

    Quick fix main 2
    Added namespace for standard use of iostream

--- PL2/main.cpp
+++ PL2/main.cpp
@@ -6,6 +6,7 @@
 /*  Mensaje de cabecera del programa principal, aqui se explicara por medio del uso de un comentario de varias lineas como funciona el codigo por encima */
 
 // Programa principal
+using namespace std;
 int main(){
   cout << "Hello, World!" << endl;
   cout << "CodeBlocks does not have any GitHub integration, therefore <<<< any IDE with GitHub management integration" << endl;
