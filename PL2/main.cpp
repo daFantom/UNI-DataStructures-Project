3ef14dcb48c4ca21c1baa13a3100534de4fc4fce
Fantom <emanuel.baciu@edu.uah.es>
2025-11-12 19:50:59 +0100

    Add placeholder main.cpp
    Creado main.cpp para hacer el pull directamente con el y sin necesidad de crear un README adicional y luego borrarlo

---
+++ PL2/main.cpp
@@ -0,0 +1,12 @@
+// Librerias y ficheros de cabecera
+# include <iostream>
+# include <ctime>
+// Fin inclusiones
+
+/*  Mensaje de cabecera del programa principal, aqui se explicara por medio del uso de un comentario de varias lineas como funciona el codigo por encima */
+
+// Programa principal
+int main(){
+  cout << "Hello, World!" << endl;
+  cout << "CodeBlocks does not have any GitHub integration, therefore <<<< any IDE with GitHub management integration" << endl;
+};
